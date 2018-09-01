71907069a313fda779e7dac426862dfb387a0466
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2018-08-31 17:47:06 -0700

    Updated script.

--- x86devirt.py
+++ x86devirt.py
@@ -2,6 +2,8 @@ from x64dbgpy.pluginsdk import *
 import x64dbgpy
 import os
 import subprocess
+import yara
+import distorm3
 
 devirtTool = os.path.join(os.path.dirname(os.path.realpath(__file__)), "x86virt-disasm.exe")
 bufferBin = os.path.join(os.path.dirname(os.path.realpath(__file__)), "buffer.bin")
@@ -79,4 +81,68 @@ def devirt(source, destination, size):
             x64dbg._plugin_logputs("Unable to resolve jump!")
     return
 
-x64dbg._plugin_logputs("Python script to use the x86virt-disassembler tool to reconstruct and automatically devirtualize protected executables. Written by Jeremy Wildsmith, github repo: https://github.com/JeremyWildsmith/x86devirt")
+def findVmStub():
+    rule = yara.compile(source='rule VmStub {strings: $hex_string = { 60 9C 9C 59 8B C4 8B 5C 24 24 8B 54 24 28 E8 00 00 00 00 5C 8B ?? ?? ?? ?? ?? ?? 55 8B EC 83 EC 2C 89 44 24 28 89 0C 24 33 C9 8D 7C 24 04 8B F2 46 8A 02 32 42 01 0F B6 C0 50 } condition: $hex_string}')
+    buffer = GetMainModuleSectionList()
+    for val in buffer:
+        x64dbg._plugin_logputs("Scanning section: " + val.name)
+        scanBuffer = Read(val.addr, val.size)
+        matches = rule.match(data=scanBuffer)
+
+        if(len(matches) <= 0):
+             continue
+        
+        matchedStrings = matches[0].strings
+
+        if(len(matchedStrings) == 1):
+            vmStubMatch = matchedStrings[0]
+            return vmStubMatch[0] + val.addr
+
+    return None
+
+def findVmStubCrossReferences(vmStub):
+    #x64dbg has not provided an interface to their cross-reference functionality yet...
+    #So... We're going to have to do this with signatures
+    references = []
+    signatureSize = 25
+    rule = yara.compile(source='rule JumpToVmStub {strings: $hex_string = { 9C 81 6C ?? ?? ?? ?? ?? ?? 9D E8 ?? ?? ?? ?? 9C 81 6C ?? ?? ?? ?? ?? ?? 9D } condition: $hex_string}')
+    buffer = GetMainModuleSectionList()
+    for val in buffer:
+        x64dbg._plugin_logputs("Scanning section: " + val.name)
+        scanBuffer = Read(val.addr, val.size)
+        matches = rule.match(data=scanBuffer)
+
+        for m in matches:
+            matchedStrings = m.strings
+            for referenceMatch in matchedStrings:
+                instructionLocation = referenceMatch[0] + signatureSize;
+                
+                lastInstructionBuffer = scanBuffer[instructionLocation : instructionLocation + 10]
+                
+                decomposedInstructions = distorm3.Decompose(instructionLocation + val.addr, lastInstructionBuffer)
+                vmReferenceInstruction = decomposedInstructions[0]
+                
+                if (vmReferenceInstruction.flowControl == "FC_UNC_BRANCH" and vmReferenceInstruction.operands[0].value == vmStub):                    
+                    references.append({"start": referenceMatch[0] + val.addr, "jump": instructionLocation})
+
+    return references
+
+def main():
+    x64dbg._plugin_logputs("Python script to use the x86virt-disassembler tool to reconstruct and automatically devirtualize protected executables. Written by Jeremy Wildsmith, github repo: https://github.com/JeremyWildsmith/x86devirt")
+    x64dbg._plugin_logputs("Scanning for the location of the VM Stub...")
+    vmStub = findVmStub()
+
+    if(vmStub is None):
+        x64dbg._plugin_logputs("Failed to locate VM Stub. Exiting...")
+        return
+
+    x64dbg._plugin_logputs("VM Stub located at: " + hex(vmStub))
+    x64dbg._plugin_logputs("Searching for cross references to VM Stub...")
+
+    references = findVmStubCrossReferences(vmStub)
+    x64dbg._plugin_logputs("Found " + str(len(references)) + " references... Emulating to get locations of encrypted sections")
+    #SetEIP
+
+    #Message("The Virtual Machine stub has been located and hooked. Invoke virturalized behaviour via user interaction and then type ")
+
+main()
